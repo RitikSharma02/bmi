d18f4081b31eeffecfe8e98126d5111636d05d21
RITIK PANDAT <pandatritik47@gmail.com>
2023-07-29 22:22:43 +0530

    updates

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,19 @@
+import 'package:bmi/reusable_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_card.dart';
+import 'icon_content.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF1D1E33);
+const inactiveCardColour = Color(0xFF111328);
+const bottomContainerColour = Color(0xFFEB1555);
+
+enum Gender {
+  male,
+  female,
+}
 
 class InputPage extends StatefulWidget {
   @override
@@ -7,77 +21,87 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Gender? selectedGender;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('BMI CALCULATOR'),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
                     child: ReusableCard(
-                      colour: Color(0xFF1D1E33),
-                    ),
-                  ),
-                  Expanded(
-                    child: Expanded(
-                      child: ReusableCard(
-                        colour: Color(0xFF1D1E33),
+                      colour: selectedGender == Gender.male
+                          ? activeCardColour
+                          : inactiveCardColour,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: "MALE",
                       ),
+                      // colour: maleCardColour,
                     ),
                   ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Expanded(
-                child: ReusableCard(
-                  colour: Color(0xFF1D1E33),
                 ),
-              ),
-            ),
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Expanded(
-                      child: ReusableCard(
-                        colour: Color(0xFF1D1E33),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                    child: ReusableCard(
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: "Female",
                       ),
+                      colour: selectedGender == Gender.female
+                          ? activeCardColour
+                          : inactiveCardColour,
                     ),
                   ),
-                  Expanded(
-                    child: Expanded(
-                      child: ReusableCard(
-                        colour: Color(0xFF1D1E33),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                )
+              ],
             ),
-          ],
-        ));
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  ReusableCard({super.key, required this.colour});
-  Color colour;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: colour,
-        // color: Color(0xFF1D1E33),
+          ),
+          // Expanded(
+          //   child: ReusableCard(
+          //     colour: activeCardColor,
+          //   ),
+          // ),
+          // Expanded(
+          //   child: Row(
+          //     children: [
+          //       Expanded(
+          //         child: ReusableCard(
+          //           colour: activeCardColor,
+          //         ),
+          //       ),
+          //       Expanded(
+          //         child: ReusableCard(
+          //           colour: activeCardColor,
+          //         ),
+          //       )
+          //     ],
+          //   ),
+          // ),
+          Container(
+            color: bottomContainerColour,
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          )
+        ],
       ),
     );
   }
---
+++ lib/reusable_card.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+class ReusableCard extends StatelessWidget {
+  // const ReusableCard({
+  //   super.key,
+  // });
+  ReusableCard({required this.colour, required this.cardChild});
+  final Color colour;
+  final Widget cardChild;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: cardChild,
+    );
+  }
+}
